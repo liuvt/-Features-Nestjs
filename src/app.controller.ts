925622528edd830ca3c333d3bdbba98d27175326
Lưu Thiện Văn <ltvan1108@gmail.com>
2020-06-25 11:01:02 +0700

    add Logger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,20 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller('/app')
 export class AppController {
-
+    protected readonly logger = new Logger(`RESULT LOGGER METHOD: ${AppController.name}`);
     constructor(private readonly service: AppService) { }
 
     //http://localhost:5000/app?a=5
     @Get()
     async jestUnitTest(@Query('a') a: number):Promise<number>{
         const rs = await this.service.jestUnitTest(a);
-        console.log(a);
         if(rs <= 0){
-            console.log(rs)
+            this.logger.log(rs);
             return 0;
         }
-        console.log(rs)
+        this.logger.log(rs);
         return rs;
     }
 }
