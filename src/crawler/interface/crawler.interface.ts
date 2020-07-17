d51c7c1441b508a62f776503c0bb7596124cafac
Lưu Thiện Văn <ltvan1108@gmail.com>
2020-07-17 09:41:22 +0700

    get OpenGraph not found return obj link

--- src/crawler/interface/crawler.interface.ts
+++ src/crawler/interface/crawler.interface.ts
@@ -1,23 +1,23 @@
-export interface IOpenGraph {
-    sitename?: string,
-    title?: string,
-    description?: string,
-    url: string,
-    type?: string,
-    image?: IImage,
-    video?: IVideo
+export interface CrawlerResponseInterface {
+    sitename?: string;
+    title?: string;
+    description?: string;
+    url: string;
+    type?: string;
+    image?: Image;
+    video?: Video;
 }
 
-export interface IImage {
-    url?: string,
-    width?: number,
-    height?: number,
-    type?: string,
+interface Image {
+    url: string;
+    width: number;
+    height: number;
+    type: string;
 }
 
-export interface IVideo {
-    url?: string,
-    width?: number,
-    height?: number,
-    type?: string,
+interface Video {
+    url: string;
+    width: number;
+    height: number;
+    type: string;
 }
